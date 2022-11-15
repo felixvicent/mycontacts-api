32b45a0766f34535dfae2417706dcdade36a1fba
Félix Vicente <felixvicent1306@gmail.com>
2022-11-15 13:48:37 -0300

    Config eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World');
-})
+  response.send('Hello World');
+});
 
+// eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
