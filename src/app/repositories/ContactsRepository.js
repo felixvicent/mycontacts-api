97b0d18fb5752a1763acf12bfe9c7b9989b72437
Félix Vicente <felixvicent1306@gmail.com>
2022-11-15 17:10:36 -0300

    Create show & delete contact functions

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Félix',
     email: 'felixvicent1306@gmail.com',
     phone: '83987081294',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +14,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
