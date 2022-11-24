ea77e392baba3cc1cfd4c20f57e53e337bb5fd58
Félix Vicente <felixvicent1306@gmail.com>
2022-11-24 13:11:42 -0300

    Create categories module

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const routes = Router();
 
@@ -9,5 +10,11 @@ routes.get('/contacts/:id', ContactController.show);
 routes.post('/contacts', ContactController.store);
 routes.put('/contacts/:id', ContactController.update);
 routes.delete('/contacts/:id', ContactController.delete);
+
+routes.get('/categories', CategoryController.index);
+routes.get('/categories/:id', CategoryController.show);
+routes.post('/categories', CategoryController.store);
+routes.put('/categories/:id', CategoryController.update);
+routes.delete('/categories/:id', CategoryController.delete);
 
 module.exports = routes;
